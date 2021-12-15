a1e7ce47cf615d9635f07123ec241830f373f43b
Nick Mark <nickmark028@gmail.com>
2021-12-15 10:22:40 +0700

    Format

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-
 import { Provider } from "react-redux";
 import store from "redux/store";
 
 import Home from "pages";
-import DetailItem  from "pages/detail";
+import DetailItem from "pages/detail";
 
 function App(): JSX.Element {
-  
   return (
-  
-      
-        // <Home />
+    // <Home />
 
-        <DetailItem />
-  
+    <DetailItem />
   );
 }
 
