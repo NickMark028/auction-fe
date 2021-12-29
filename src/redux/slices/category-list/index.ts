d3dc24a30025311afcc3ad34685853593248f69e
Nick Mark <nickmark028@gmail.com>
2021-12-29 12:34:30 +0700

    Build a generic add case for thunk builder

--- src/redux/slices/category-list/index.ts
+++ src/redux/slices/category-list/index.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { TCategoryList, TState } from "models";
+import { addThunkBuilderCases } from "redux/thunk";
 import { getCategoryListTC } from "./getCategoryList";
 
 const initialState: TState<TCategoryList> = {
@@ -11,20 +12,20 @@ const categoryListSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers(builder) {
-    builder
-      .addCase(getCategoryListTC.pending, (state, action) => {
-        state.errorMsg = undefined;
-        state.status = 'pending';
-      })
-      .addCase(getCategoryListTC.fulfilled, (state, action) => {
-        state.data = action.payload;
-        state.status = 'success';
-      })
-      .addCase(getCategoryListTC.rejected, (state, action) => {
-        state.errorMsg = action.error.message;
-        state.status = 'reject';
-      })
-
+    addThunkBuilderCases(builder, getCategoryListTC);
+    // builder
+    //   .addCase(getCategoryListTC.pending, (state, action) => {
+    //     state.errorMsg = undefined;
+    //     state.status = 'pending';
+    //   })
+    //   .addCase(getCategoryListTC.fulfilled, (state, action) => {
+    //     state.data = action.payload;
+    //     state.status = 'success';
+    //   })
+    //   .addCase(getCategoryListTC.rejected, (state, action) => {
+    //     state.errorMsg = action.payload;
+    //     state.status = 'reject';
+    //   })
   }
 });
 
--- src/redux/slices/product-search-list/index.ts
+++ src/redux/slices/product-search-list/index.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { TCategoryList, TProductSearchList, TState } from "models";
+import { TProductSearchList, TState } from "models";
+import { addThunkBuilderCases } from "redux/thunk";
 import { searchProductTC } from "./searchProduct";
 
 const initialState: TState<TProductSearchList> = {
@@ -11,20 +12,20 @@ const productSearchList = createSlice({
     initialState,
     reducers: {},
     extraReducers(builder) {
-        builder
-            .addCase(searchProductTC.pending, (state, action) => {
-                state.errorMsg = undefined;
-                state.status = 'pending';
-            })
-            .addCase(searchProductTC.fulfilled, (state, action) => {
-                state.data = action.payload;
-                state.status = 'success';
-            })
-            .addCase(searchProductTC.rejected, (state, action) => {
-                state.errorMsg = action.payload;
-                state.status = 'reject';
-            })
-
+        addThunkBuilderCases(builder, searchProductTC);
+        // builder
+        //     .addCase(searchProductTC.pending, (state, action) => {
+        //         state.errorMsg = undefined;
+        //         state.status = 'pending';
+        //     })
+        //     .addCase(searchProductTC.fulfilled, (state, action) => {
+        //         state.data = action.payload;
+        //         state.status = 'success';
+        //     })
+        //     .addCase(searchProductTC.rejected, (state, action) => {
+        //         state.errorMsg = action.payload;
+        //         state.status = 'reject';
+        //     })
     }
 });
 
