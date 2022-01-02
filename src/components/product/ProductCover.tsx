b31c58ff0c0e1dae099ca6d7a6746d23fdb38e74
Nick Mark <nickmark028@gmail.com>
2022-01-02 14:59:32 +0700

    Link with product detail

--- src/components/product/ProductCover.tsx
+++ src/components/product/ProductCover.tsx
@@ -3,36 +3,40 @@ import { TProduct } from 'models';
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
-interface Props extends TProduct {}
+interface Props extends TProduct { }
 
 const ProductCover = (props: Props) => {
   const { id, coverImageUrl, currentPrice, topBidder, name, auctionLogCount } =
     props;
 
   return (
-    <div className="col-lg-2 col-md-4 col-sm-6">
+    <div className="col-lg-3 col-md-6 col-sm-12">
       <div className="product__item">
-        <div
-          className="product__item__pic set-bg"
-          style={{
-            backgroundImage: `url(${coverImageUrl})`,
-            width: '100%',
-          }}
-        >
-          <ul className="product__item__pic__hover">
-            <li>
-              <a href="#">
-                <i className="fa fa-heart" />
-              </a>
-            </li>
 
-            <li>
-              <a href="#">
-                <i className="fa fa-gavel" />
-              </a>
-            </li>
-          </ul>
-        </div>
+        <Link to={PageURL.Detail.replace(':id', id.toString())}>
+          <div
+            className="product__item__pic set-bg"
+            style={{
+              backgroundImage: `url(${coverImageUrl})`,
+              width: '100%',
+            }}
+          >
+            <ul className="product__item__pic__hover">
+              <li>
+                <a href="#">
+                  <i className="fa fa-heart" />
+                </a>
+              </li>
+
+              <li>
+                <a href="#">
+                  <i className="fa fa-gavel" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </Link>
+
         <div className="product__item__text">
           <h6>
             <a href="#">{name}</a>
@@ -43,6 +47,7 @@ const ProductCover = (props: Props) => {
           </h6>
           <h6>Count: {auctionLogCount}</h6>
         </div>
+
       </div>
     </div>
   );
