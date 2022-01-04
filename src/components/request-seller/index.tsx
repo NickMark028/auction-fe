218df118195aec4100ffd6b940b744c133d4156c
thanhvu2901 <vcthanh2901@gmail.com>
2022-01-04 22:20:12 +0700

    role profile

--- src/components/request-seller/index.tsx
+++ src/components/request-seller/index.tsx
@@ -1,20 +1,41 @@
 import React, { useEffect, useState } from 'react';
 
 import axiosClient from 'utils/axiosClient';
+
 export const RequestToSeller: React.FC = () => {
-  return (
-    <>
-      <h5>Do you want to become seller? </h5>
-      <button
-        onClick={() => {
-          axiosClient.post(
-            `/api/bidder/changeRole/${localStorage.getItem('auction-user-id')}`
-          );
-          window.alert('WELCOM NEW SELLER!!');
-        }}
-      >
-        YES
-      </button>
-    </>
-  );
+  const [checkrole, setRole] = useState([]);
+  useEffect(() => {
+    setTimeout(async () => {
+      async function role() {
+        return await axiosClient.get(
+          `/api/seller/checkrole/${localStorage.getItem('auction-user-id')}`
+        );
+      }
+      role().then((res) => {
+        setRole(res.data);
+      });
+    });
+  }, []);
+
+  if (checkrole) {
+    return <h5>You are a seller </h5>;
+  } else
+    return (
+      <>
+        <h5>Do you want to become seller? </h5>
+        <button
+          onClick={() => {
+            axiosClient.post(
+              `/api/bidder/changeRole/${localStorage.getItem(
+                'auction-user-id'
+              )}`
+            );
+            window.alert('WAIT admin approve!!');
+            window.location.reload();
+          }}
+        >
+          YES
+        </button>
+      </>
+    );
 };
--- src/components/seller-product/index.tsx
+++ src/components/seller-product/index.tsx
@@ -3,6 +3,7 @@ import axiosClient from 'utils/axiosClient';
 import moment from 'moment';
 export const SellerProduct: React.FC = () => {
   const [sellerProduct, setSellerProduct] = useState([]);
+  const [checkrole, setCheckRole] = useState([]);
   useEffect(() => {
     setTimeout(async () => {
       axiosClient
@@ -12,41 +13,59 @@ export const SellerProduct: React.FC = () => {
           )}`
         )
         .then((res) => setSellerProduct(res.data));
+
+      async function role() {
+        return await axiosClient.get(
+          `/api/seller/checkrole/${localStorage.getItem('auction-user-id')}`
+        );
+      }
+
+      role().then((res) => {
+        setCheckRole(res.data);
+      });
     });
   }, []);
-  return (
-    <>
-      <div className="product__details__tab__desc" style={{ overflow: 'auto' }}>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>Bidder Name</th>
-              <th>Price</th>
-              <th>Current Price</th>
-              <th>Auction Log Count</th>
-              <th>Bidder count</th>
-              <th>Bid At</th>
-            </tr>
-          </thead>
-          <tbody id="category-container">
-            {sellerProduct?.map((auctionLog, index) => (
-              <tr key={index}>
-                <td>{auctionLog.name}</td>
-                <td> {auctionLog.reservedPrice} </td>
-                <td> {auctionLog.currentPrice} </td>
-                <td>{auctionLog.auctionLogCount}</td>
-                <td>{auctionLog.bidderCount}</td>
-                <td>
-                  {' '}
-                  {moment(auctionLog.createdAt).format(
-                    'MMMM Do YYYY, h:mm:ss a'
-                  )}
-                </td>
+  console.log(checkrole);
+  if (!checkrole) {
+    return <h5>You are not a seller!! Please become a seller! </h5>;
+  } else {
+    return (
+      <>
+        <div
+          className="product__details__tab__desc"
+          style={{ overflow: 'auto' }}
+        >
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>Bidder Name</th>
+                <th>Price</th>
+                <th>Current Price</th>
+                <th>Auction Log Count</th>
+                <th>Bidder count</th>
+                <th>Bid At</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+            </thead>
+            <tbody id="category-container">
+              {sellerProduct?.map((auctionLog, index) => (
+                <tr key={index}>
+                  <td>{auctionLog.name}</td>
+                  <td> {auctionLog.reservedPrice} </td>
+                  <td> {auctionLog.currentPrice} </td>
+                  <td>{auctionLog.auctionLogCount}</td>
+                  <td>{auctionLog.bidderCount}</td>
+                  <td>
+                    {' '}
+                    {moment(auctionLog.createdAt).format(
+                      'MMMM Do YYYY, h:mm:ss a'
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
 };
