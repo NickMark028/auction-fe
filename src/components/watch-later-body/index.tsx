2d2cb2d2a529aa36cdb3942084aea87040d87c20
Nick Mark <nickmark028@gmail.com>
2022-01-16 11:42:18 +0700

    Fix style for spinner

--- src/components/watch-later-body/index.tsx
+++ src/components/watch-later-body/index.tsx
@@ -1,11 +1,12 @@
 import ServerError from 'components/500-server-error';
+import Loading from 'components/loading';
 import ProductRow from 'components/product/ProductRow';
 import { TProduct, TStatus } from 'models';
 import React, { FC, useEffect, useState } from 'react';
-import { Col, Row, Spinner } from 'react-bootstrap';
+import { Col, Container, Row, Spinner } from 'react-bootstrap';
 import { getWatchList } from './api';
 
-interface Props {}
+interface Props { }
 
 const WatchLaterBody: FC<Props> = (props: Props) => {
   const [status, setStatus] = useState<TStatus>('idle');
@@ -29,52 +30,55 @@ const WatchLaterBody: FC<Props> = (props: Props) => {
 
   const uiMap = {
     idle: undefined,
-    pending: <Spinner animation="border" />,
+    pending: <Loading />,
     success: (
-      <>
-        <section>
-          <h2 className="h2 mt-4">Watch later</h2>
-        </section>
-
-        <section className="">
-          {status === 'success' && watchList && (
-            <>
-              {watchList.length !== 0 ? (
-                watchList.map((product, index) => (
-                  <ProductRow
-                    key={product.id}
-                    productId={product.id}
-                    imageUrl={product.coverImageUrl}
-                    name={product.name}
-                    description={product.description}
-                    pricing={product.currentPrice}
-                    timeExpired={product.timeExpired}
-                    onProductRemoved={(productId) =>
-                      setWatchList(
-                        watchList.filter((product) => product.id !== productId)
-                      )
-                    }
+      <section className="">
+        {status === 'success' && watchList && (
+          <>
+            {watchList.length !== 0 ? (
+              watchList.map((product, index) => (
+                <ProductRow
+                  key={product.id}
+                  productId={product.id}
+                  imageUrl={product.coverImageUrl}
+                  name={product.name}
+                  description={product.description}
+                  pricing={product.currentPrice}
+                  timeExpired={product.timeExpired}
+                  onProductRemoved={(productId) =>
+                    setWatchList(
+                      watchList.filter((product) => product.id !== productId)
+                    )
+                  }
+                />
+              ))
+            ) : (
+              <Row>
+                <Col className="d-flex flex-column mb-5 align-items-center">
+                  <img
+                    className="my-5"
+                    src="./asset/img/empty.png"
+                    style={{ maxWidth: '40%' }}
                   />
-                ))
-              ) : (
-                <Row>
-                  <Col className="d-flex flex-column mb-5 align-items-center">
-                    <img
-                      className="my-5"
-                      src="./asset/img/empty.png"
-                      style={{ maxWidth: '40%' }}
-                    />
-                  </Col>
-                </Row>
-              )}
-            </>
-          )}
-        </section>
-      </>
+                </Col>
+              </Row>
+            )}
+          </>
+        )}
+      </section>
     ),
     reject: <ServerError />,
   };
 
-  return <div className="container">{uiMap[status]}</div>;
+  return (
+    <Container>
+      <section>
+        <h2 className="h2 mt-4">Watch later</h2>
+      </section>
+
+      <div className="container">{uiMap[status]}</div>;
+    </Container>
+  )
 };
+
 export default WatchLaterBody;
