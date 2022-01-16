e663027b7981e8449630e6d5e0b6682e267f5709
Nick Mark <nickmark028@gmail.com>
2022-01-16 09:38:13 +0700

    Update AdminRoute.tsx

--- src/routers/AdminRoute.tsx
+++ src/routers/AdminRoute.tsx
@@ -2,16 +2,16 @@ import { PageURL } from 'enum/PageURL';
 import { FC, useEffect } from 'react';
 import { Redirect, Route, RouteProps, useHistory } from 'react-router-dom';
 
-interface Props extends RouteProps {}
+interface Props extends RouteProps { }
 
 const AdminRoute: FC<Props> = (props: Props) => {
   const { path, exact, strict, component, location } = props;
 
   const accessToken = localStorage.getItem('auction-user-token');
-  var hasAccess =  false;
+  var hasAccess = false;
   const role = localStorage.getItem('auction-user-role')
-  if(role=='admin'){
-    hasAccess=true
+  if (role == 'admin') {
+    hasAccess = true
   }
   return hasAccess ? (
     <Route path={path} exact={exact} strict={strict} component={component} />
