0c32291c6324ebdcc749dbaf661574cc2730a83e
Nick Mark <nickmark028@gmail.com>
2022-01-20 11:33:41 +0700

    Fix routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-
-import { PublicRoute } from 'routers';
-import { PageURL } from 'enum/PageURL';
-import PrivateRoute from 'routers/PrivateRoute';
-import { AdminRoute } from 'routers';
-import { DetailItem } from 'pages';
-import loginItem from 'pages/login';
-import userList from 'pages/userlist';
-import regItem from 'pages/register';
-import Admin from 'pages/admin';
-import profileItem from 'pages/profile';
-import { HomePage, SearchPage } from 'pages';
 import store from 'redux/store';
-import WatchListPage from 'pages/watch-list';
+import RootRouter from 'routers/RootRouter';
 
 function App(): JSX.Element {
   return (
     <Provider store={store}>
-      <Router>
-        <Switch>
-          <PublicRoute exact={true} path={PageURL.Home} component={HomePage} />
-          <PublicRoute path={PageURL.Search} component={SearchPage} />
-          <PublicRoute path={PageURL.Detail} component={DetailItem} />
-          <PublicRoute path={PageURL.Login} component={loginItem} />
-          <PublicRoute path={PageURL.List} component={userList} />
-          <PublicRoute path={PageURL.Register} component={regItem} />
-          <PrivateRoute path={PageURL.WatchList} component={WatchListPage} />
-          <AdminRoute path={PageURL.Admin} component={Admin} />
-          <PrivateRoute path={PageURL.Profile} component={profileItem} />
-        </Switch>
-      </Router>
+      <RootRouter />
     </Provider>
   );
 }
